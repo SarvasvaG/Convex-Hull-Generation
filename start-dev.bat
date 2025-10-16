991485ff049fdde038d4393a206df4a4305fd43d
Sarvasva <sarvasva_g@cs.iitr.ac.in>
2025-10-16 18:24:14 +0530

    chore(build): remove legacy run scripts and unncessary files

--- start-dev.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting Convex Hull Visualization development server...
-echo.
-echo The application will open in your browser at http://localhost:5173
-echo.
-echo Press Ctrl+C to stop the server
-echo.
-
-call npm run dev
